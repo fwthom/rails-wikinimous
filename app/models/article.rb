37f89c16b62a97fdf1fc2dd1b113b00f6df0cdb4
Thomas Barbieux <thomas@fresh.win>
2024-11-12 16:03:40 +0100

    Yalla

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,11 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :content, presence: true
+  before_save :make_title_pretty
+
+  private
+
+  def make_title_pretty
+    self.title = title.titleize
+  end
 end
